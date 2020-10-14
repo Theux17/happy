2f23e7143a6b02ea3d79131332a9b8841d149766
Theux17 <oliveira.ms18@gmail.com>
2020-10-14 19:06:44 -0300

    criando orfanato e abstraindo em controller

--- back-end/src/database/migrations/1602704568523-create_orphanages.ts
+++ back-end/src/database/migrations/1602704568523-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1602704568523 implements MigrationInterface {
             type: 'text',
           },
           {
+            name: 'opening_hours',
+            type: 'varchar',
+          },
+          {
             name: 'open_on_weekends',
             type: 'boolean',
             default: false,
